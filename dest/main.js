6a4f6b6ac06db37d566a0fd9913db0f28d40d683
DucTaiNguyen <brucelee50002000pro@gmail.com>
2021-05-08 15:03:05 +0700

    first commit

--- dest/main.js
+++ dest/main.js
@@ -1,174 +1,74 @@
 console.log('Hello CFD');
 
-let nav = document.querySelector('.nav');
-let btnmenu = document.querySelector('header .btnmenu');
+// let $carousel = $('.slider__item-wrap');
+// $carousel.flickity({
+//     //options
+//     cellAlign: 'left',
+//     contain: true,
+//     wrapAround: true,
+//     prevNextButtons: false,
+//     // pageDots: false,
+//     on: {
+//         ready: function () {
+//             // console.log('flicktify');
+//             let dotted = $('.flickity-page-dots'); // Tao DOM ao
+//             // console.log(dotted);
+//             paging = $('.paging .paging__dotted');
+//             dotted.appendTo(paging);
+//         },
+//         change: function (index) {
+//             let number = $('.paging .paging__number');
+//             let indexPage = index + 1;
+//             // console.log(index);
+//             number.text(indexPage.toString().padStart(2, 0));
+//             // console.log(number);
+//         }
+//     }
+// })
 
-// btnmenu.addEventListener('click', toggleMenu);
-btnmenu.onclick = function toggleMenu() {
-    nav.classList.toggle('active');
-    this.classList.toggle('active');
-}
+// $('.control .prev').on('click', function () {
+//     console.log('dawdaw')
+//     $carousel.flickity('previous');
+// })
+// $('.control .next').on('click', function () {
+//     $carousel.flickity('next');
+// })
 
-
-//
-window.addEventListener('resize', function () {
-    let positionResize = window.innerWidth;
-    if (positionResize > 985) {
-        nav.classList.remove('active')
-    }
-    // console.log(positionResize)
-})
-
-
-
-///#################################################################################################
-
-let lang = document.querySelector('.lang')
-let langCurrent = document.querySelector('.lang .lang__current span');
-let langOpt = document.querySelector('.lang .lang__option');
-let langItems = document.querySelectorAll('.lang .lang__option a')
-lang.addEventListener('click', function (e) {
-    e.stopPropagation();
-    lang.classList.toggle('active');
-    // console.log(lang);
-});
-
-
-
-langItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-        let langText = this.textContent;
-        let langCurrentSpan = langCurrent.textContent;
-        langCurrent.innerHTML = langText;
-        this.innerHTML = langCurrentSpan;
-        // console.log(langCurrentSpan);
-    })
-})
-
-document.addEventListener('click', function () {
+// $('.main-carousel').flickity({
+//     // options
+//     cellAlign: 'left',
+//     contain: true,
+//     wrapAround: true,
+//     prevNextButtons: false,
+// });
 
 
-    lang.classList.remove('active');
-})
-
-
-//###########################################################################
-
-let menu = document.querySelectorAll('header .menu a');
-let heightHeader = document.querySelector('header').offsetHeight;//chieu cao va padding
-let sections = [];
-
-function removeActiveMenu() {
-    menu.forEach(function (menu_element) {
-        menu_element.classList.remove('active');
-    })
-}
-
-menu.forEach(function (element, index) {
-    let className = element.getAttribute('href').replace('#', "");
-    let section = document.querySelector('.' + className);
-    sections.push(section);
-    element.addEventListener('click', function (e) {
-
-        e.preventDefault();
-        let positionSection = section.offsetTop;
-        window.scrollTo({
-            top: positionSection - heightHeader + 1,
-            behavior: 'smooth'
-        })
-        // removeActiveMenu();
-        // element.classList.add('active');
-    })
-})
-
-window.addEventListener('scroll', function () {
-    let positionScroll = window.pageYOffset;
-    // console.log(positionScroll);
-    sections.forEach(function (section, index) {
-
-        if (positionScroll > section.offsetTop - heightHeader && positionScroll < section.offsetTop + section.offsetHeight) {
-            removeActiveMenu();
-            menu[index].classList.add('active');
-        } else {
-            menu[index].classList.remove('active');
-
-        }
-    })
-})
-
-//#########################################################################################################################################
-// Back to To
-let backtotop = document.querySelector('.back-to-top');
-backtotop.addEventListener('click', function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-})
-let positionSectionProduct = document.querySelector('.product').offsetTop;
-window.addEventListener('scroll', function () {
-    let positionScroll = window.pageYOffset;
-    // console.log(positionScroll);
-    if (positionScroll > positionSectionProduct) {
-        backtotop.style.display = 'block';
-
-    } else {
-        backtotop.style.display = 'none';
-    }
-})
-
-////########## POUP VIDEOS #####################################
-
-let button_videos = document.querySelectorAll('.play_button');
-let popup_videos = document.querySelector('.popup-video');
-let close_popup_video = document.querySelector('.popup-video .close');
-let iframe = document.querySelector('.popup-video iframe');
-button_videos.forEach(function (button) {
-    // console.log(button);
-    button.addEventListener('click', function () {
-        let video_id = button.getAttribute('data-video-id');
-        iframe.setAttribute('src', "https://www.youtube.com//embed/" + video_id);
-        console.log(video_id);
-        popup_videos.style.display = 'flex';
-    })
-})
-
-close_popup_video.addEventListener('click', function () {
-    iframe.setAttribute('src', '');
-    popup_videos.style.display = 'none';
-})
-
-document.querySelector('.popup-video').addEventListener('click', function (event) {
-
-    // console.log(event);
-    iframe.setAttribute('src', '');
-    popup_videos.style.display = 'none';
-})
-
-//############################## Slider ###########################
-// 
-
-let listItemSlider = document.querySelectorAll('.slider__item');
+let listItemSlider = $('.slider__item');
 let currentSlider = 0;
-let number = document.querySelector('.paging__number');
+let number = $('.paging__number');
+let dot_li = $('.paging__dotted ol li');
 
-listItemSlider.forEach(function (itemSlider, index) {
-    if (itemSlider.classList.contains('active')) {
-        currentSlider = index;
-    }
-})
+listItemSlider.each(function (index) {
+
+    currentSlider = $(this).hasClass('active') ? index : currentSlider;
+}
+)
+
+function showNumber(index) {
+    number.html((index).toString().padStart(2, '0'));
+}
+
+// Mac Dinh
+showNumber(currentSlider + 1);
 
 
-number.innerHTML = (currentSlider + 1).toString().padStart(2, '0');
-console.log(number);
-
-
+$(dot_li[currentSlider]).addClass('is-selected');
 
 
 
 
 // console.log(currentSlider);
-document.querySelector('.control__btn.next').addEventListener('click', function () {
+$(document).on('click', '.control__btn.next', function () {
     if (currentSlider < listItemSlider.length - 1) {
         // listItemSlider[currentSlider].classList.remove('active');
         // listItemSlider[currentSlider + 1].classList.add('active');
@@ -186,7 +86,7 @@ document.querySelector('.control__btn.next').addEventListener('click', function 
 
 })
 
-document.querySelector('.control__btn.prev').addEventListener('click', function () {
+$(document).on('click', '.control__btn.prev', function () {
     // console.log(1);
     if (currentSlider > 0) {
         // listItemSlider[currentSlider].classList.remove('active');
@@ -204,7 +104,14 @@ document.querySelector('.control__btn.prev').addEventListener('click', function 
 })
 
 function goTo(index) {
-    listItemSlider[currentSlider].classList.remove('active');
-    listItemSlider[index].classList.add('active');
+    $(listItemSlider[currentSlider]).removeClass('active'); // remove truoc, roi add sau
+    $(listItemSlider[index]).addClass('active');
+    $(dot_li[currentSlider]).removeClass('is-selected');
+    $(dot_li[index]).addClass('is-selected');
     currentSlider = index;
+    showNumber(currentSlider + 1);
 }
+
+dot_li.click(function () {
+    goTo($(this).index());
+})
